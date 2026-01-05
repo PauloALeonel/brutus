2e81728fd28a2f5a3b62436808f37a9639362b50
taila01 <142836417+taila01@users.noreply.github.com>
2026-01-05 15:44:41 -0300

    #fix# limpando codigo

--- carrinho/cep.js
+++ carrinho/cep.js
@@ -1,22 +1,22 @@
-function buscaCep(){
+function buscaCep() {
     let cep = document.getElementById('CEP').value;
-    if(cep!==""){
+
+    if (cep !== "") {
         let url = "https://brasilapi.com.br/api/cep/v1/" + cep;
         let req = new XMLHttpRequest();
+
         req.open("GET", url);
         req.send();
 
-        req.onload = function(){
-            if(req.status===404){
+        req.onload = function() {
+            if (req.status === 404) {
                 alert("CEP inválido");
             }
- 
-            
         }
     }
 }
 
-window.onload = function(){
+window.onload = function() {
     let txtCEP = document.getElementById("CEP");
     txtCEP.addEventListener("blur", buscaCep);
 }
